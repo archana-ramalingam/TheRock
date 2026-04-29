5471a43bfded1c36d61532a2d50b7dfb6b17d4b5
Marius Brehler <marius.brehler@amd.com>
2026-04-30 00:51:50 +0200

    Add rocm-bootstrap to allowed list

--- build_tools/third_party/s3_management/manage.py
+++ build_tools/third_party/s3_management/manage.py
@@ -96,6 +96,7 @@ if CUSTOM_PREFIX:
 PACKAGE_ALLOW_LIST = {x.lower() for x in [
     # ---- ROCm ----
     "rocm",
+    "rocm_bootstrap",
     "rocm_sdk",
     "rocm_sdk_core",
     "rocm_sdk_devel",
